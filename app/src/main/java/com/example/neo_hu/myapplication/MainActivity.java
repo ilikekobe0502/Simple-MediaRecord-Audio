59594e9dbb1d8c0a468bcdd2b5b524354501abe3
NeoHu <ilikekobe0502@hotmail.com>
2015-11-03 18:03:40 +0800

    1.新增:刪除檔案權限 2.新增:Timer計時器,顯示錄幾秒,判斷時間過短部儲存

--- app/src/main/java/com/example/neo_hu/myapplication/MainActivity.java
+++ app/src/main/java/com/example/neo_hu/myapplication/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.neo_hu.myapplication;
 
 import android.annotation.TargetApi;
-import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Build;
 import android.os.Environment;
 import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -21,7 +21,8 @@ import android.widget.Toast;
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -31,10 +32,15 @@ public class MainActivity extends ActionBarActivity {
 
     private String str;
 
+    private int tsec=0,csec=0,cmin=0;
+
     MediaRecorder recorder;
     String filepath="/我的錄音檔/";
     String filename ;
     File file;
+
+    //宣告Timer
+    Timer mTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,11 +81,13 @@ public class MainActivity extends ActionBarActivity {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 switch (event.getAction()){
-                    case MotionEvent.ACTION_DOWN:
+                    case MotionEvent.ACTION_DOWN://按下時
                         NowTime();
                         file=new File(sdcardpath + filepath + filename);
 
                         mImageButton_mic.setBackground(getResources().getDrawable(R.drawable.mic_click));
+
+                        //設定檔案格式、編碼、資訊
                         recorder=new MediaRecorder();
                         //設定聲音來源
                         recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
@@ -91,28 +99,49 @@ public class MainActivity extends ActionBarActivity {
                         recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
 
 
+                        //Timer設置
+
+                        mTime = new Timer();
+                        Mytask task=new Mytask();
+                        //設定Timer(task為執行內容，0代表立刻開始,間格1秒執行一次)
+                        mTime.schedule(task, 0, 1000);
+
                         // prepare()完成錄音之準備工作，必須在try catch下完成
                         try {
                             recorder.prepare();
                             recorder.start();
-                            Log.d("Status:","錄音中.." + recorder);
+                            Log.d("Status:","錄音中.." );
 
                         } catch (Exception e) {
                             // TODO Auto-generated catch block
                             e.printStackTrace();
-                            Log.d("Status:", "錄音失敗.." + recorder.toString());
+                            Log.d("Status:", "錄音失敗..");
                         }
                         break;
-                    case MotionEvent.ACTION_UP:
+                    case MotionEvent.ACTION_UP://放開時
 
                         //記得停止撥放時要釋放資源
                         if(recorder!=null){
                             mImageButton_mic.setBackground(getResources().getDrawable(R.drawable.mic));
+                            if(mTime != null){
+                                mTime.cancel();//取消Task
+                            }
 
+
+                            //釋放紀錄器
                             recorder.stop();
                             recorder.reset();
                             recorder.release();
                             recorder = null;
+
+                            if(tsec < 2) {//小於兩秒就刪除資料不儲存
+                                file.delete();
+                                Toast.makeText(MainActivity.this, "取消錄音", Toast.LENGTH_SHORT).show();
+                            }
+
+                            tsec=0;
+                            //TextView 初始化
+                            mTextView_count.setText("");
                             Log.d("Status:", "錄音結束..." + filename);
                         }
                         break;
@@ -130,6 +159,53 @@ public class MainActivity extends ActionBarActivity {
         str = formatter.format(curDate);
         filename = str + ".aac";
     }
+
+    /**
+     * TimerTask
+     */
+    private class Mytask extends TimerTask{
+        @Override
+        public void run() {
+            // TODO Auto-generated method stub
+                //如果startflag是true則每秒tsec+1
+                tsec++;
+                Message message = new Message();
+                //傳送訊息1
+                message.what =1;
+                handler.sendMessage(message);
+        }
+    }
+
+    /**
+     *TimerTask無法直接改變元件因此要透過Handler來當橋樑
+     */
+    private Handler handler = new Handler(){
+        public  void  handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch(msg.what){
+                case 1:
+                    csec=tsec%60;
+                    cmin=tsec/60;
+                    String s="";
+                    if(cmin <10){
+                        s="0"+cmin;
+                    }else{
+                        s=""+cmin;
+                    }
+                    if(csec < 10){
+                        s=s+":0"+csec;
+                    }else{
+                        s=s+":"+csec;
+                    }
+
+                    //s字串為00:00格式
+                    mTextView_count.setText(s);
+                    Log.d("時間經過",s.toString());
+                    break;
+            }
+        }
+    };
+
 
 
     @Override
